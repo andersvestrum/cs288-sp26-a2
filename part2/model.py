4ece5c850279409fe18bd82bf1688287519370d6
andersvestrum <anders03.vestrum@gmail.com>
2026-02-16 10:51:18 -0800

    weight tying in transformerLM

--- part2/model.py
+++ part2/model.py
@@ -725,8 +725,9 @@ class TransformerLM(nn.Module):
         # Final layer norm
         self.final_ln = RMSNorm(d_model, eps)
         
-        # Output projection (to vocab size)
+        # Output projection, weight-tied with embeddings
         self.output = Linear(d_model, vocab_size)
+        self.output.weight = self.token_embeddings.weight
     
     def forward(self, token_ids: Tensor, token_positions: Optional[Tensor] = None) -> Tensor:
         """
